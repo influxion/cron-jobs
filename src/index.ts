8e4bddcc44e6c72740b2f5efb3750bbd8eeacf13
Jordon Nichols <nicholsjordon721@gmail.com>
2023-05-12 23:19:05 -0500

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,55 @@
 #!/usr/bin/env ts-node
 import * as dotenv from 'dotenv';
-dotenv.config();
 import cron from 'node-cron';
 import chalk from 'chalk';
 import puppeteer from 'puppeteer';
+import * as importReadline from 'readline';
+dotenv.config();
 
 const GITHUB_LOGIN_URL = `${process.env.GITHUB_URL!}/login`;
 const GITHUB_REPO_URL = `${process.env.GITHUB_URL!}/${process.env
   .GITHUB_USERNAME!}/${process.env.GITHUB_REPO!}/new/main`;
+
+async function auth() {
+  const readline = importReadline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250,
+    args: ['--no-sandbox'],
+  });
+  const page = await browser.newPage();
+
+  await page.goto(GITHUB_LOGIN_URL, { waitUntil: 'networkidle2' });
+
+  // Please replace 'your_username' and 'your_password' with your actual username and password.
+  await page.type('#login_field', process.env.GITHUB_USERNAME!, { delay: 100 });
+  await page.type('#password', process.env.GITHUB_PASSWORD!, { delay: 100 });
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('input[type="submit"]'),
+  ]);
+
+  let code: string = await new Promise((resolve) => {
+    readline.question('Code: ', (codeInput) => {
+      resolve(codeInput);
+      readline.close();
+    });
+  });
+
+  await page.keyboard.type(code, { delay: 100 });
+
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('button[type="submit"]'),
+  ]);
+
+  run();
+}
 
 async function run() {
   const browser = await puppeteer.launch({
@@ -66,10 +108,10 @@ async function run() {
   await browser.close();
 }
 
-run().catch(console.error);
+auth().catch(console.error);
 console.log('STARTED');
 
-cron.schedule(`0 12 * * *`, async () => {
-  console.log(chalk.bold('Cron task initiated'));
-  run();
-});
+// cron.schedule(`0 12 * * *`, async () => {
+//   console.log(chalk.bold('Cron task initiated'));
+//   run();
+// });
